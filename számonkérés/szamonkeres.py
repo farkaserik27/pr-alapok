166f2608ceb59af82269f848c1ed993a6c68d5e4
farkaserik27 <128583563+farkaserik27@users.noreply.github.com>
2023-10-12 09:28:08 +0200

    Add files via upload

---
+++ számonkérés/szamonkeres.py
@@ -0,0 +1,18 @@
+#Farkas Erik 10/D
+#szamonkeres_001.py
+"""
+Be kérek egy osztályzatot 1-5 irasuk ki a megadott jegyet értékel és szövegesen,
+ha nem felelöjegyet adtam akkor irsuk ki a jegy értékét és mellé azt a szöveget hogy nem megfelelő
+"""
+szam = int(input("Írj egy osztályzatot: "))
+if szam: 5
+print(f"A te jegyed egy {szam}")
+if szam: 4
+print(f"A te jegyed egy {szam}")
+if szam: 3
+print(f"A te jegyed egy {szam}")
+if szam: 2
+print(f"A te jegyed egy {szam}")
+if szam: 1
+print(f"A te jegyed egy {szam}")
+
