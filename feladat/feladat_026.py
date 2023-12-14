fcbb13f4a1aa57ac8f66e73e063510e0efe77831
farkaserik27 <128583563+farkaserik27@users.noreply.github.com>
2023-12-14 10:31:11 +0100

    Add files via upload

---
+++ feladat/feladat_026.py
@@ -0,0 +1,8 @@
+#feladat_026
+
+tantargyak = ['matek', 'töri', 'bio', 'kémia', 'info']
+
+index = 0
+for tantargy in tantargyak:
+    print(f'Az index: {index} A tantargy neve: {tantargy}')
+    index = index + 1
---
+++ feladat/feladat_027.py
@@ -0,0 +1,2 @@
+for szam in range(-8,10,3):
+    print(szam, end=' ')
