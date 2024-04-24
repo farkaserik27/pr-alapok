c9e834a295051710a7f7cda41561bd23635d8a7f
farkaserik27 <128583563+farkaserik27@users.noreply.github.com>
2024-04-24 07:41:45 +0200

    Delete éretségi_feladatok/nagyobb.py

--- éretségi_feladatok/nagyobb.py
+++
@@ -1,8 +0,0 @@
-szam = int(input("Adj meg egy számot! "))
-szam1 = int(input("Adj meg egy másik számot! "))
-if szam > szam1:
-    print("A nagyobb érték: {szam1}")
-elif szam < szam1:
-    print("A nagyobb érték: {szam}")
-else:
-    print("A két szám egyenlő")
